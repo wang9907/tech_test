70528ab5cf76b8b90a33aea6c5c51cc986ea20fc
wangridong <wangridong@guokang.com>
2017-11-28 23:04:16 +0800

    update

---
+++ tech-test/src/main/java/com/summer/tech/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.summer.tech.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value= "/hello",method = RequestMethod.GET)
+	public String hello(){
+		return "Srping boot hello!";
+	}
+}
