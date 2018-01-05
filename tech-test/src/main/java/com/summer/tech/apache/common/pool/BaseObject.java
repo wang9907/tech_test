f0f09d0cfbf3606155c862a185d30584b321e745
wangridong <wangridong@guokang.com>
2018-01-05 17:35:21 +0800

    例子

---
+++ tech-test/src/main/java/com/summer/tech/apache/common/pool/BaseObject.java
@@ -0,0 +1,30 @@
+package com.summer.tech.apache.common.pool;
+
+public class BaseObject {
+
+	// 记录从池中取出次数
+	private int num;
+	private boolean active;
+
+	public BaseObject() {
+		active = true;
+		System.out.println("new BaseObject!!!!!");
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+}
