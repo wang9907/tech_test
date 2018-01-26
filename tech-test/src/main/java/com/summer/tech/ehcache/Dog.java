e3cd8e7b74f8961dc88da17ec1b56afc82008aa8
wangridong <wangridong@guokang.com>
2018-01-26 15:29:58 +0800

    增加ehcache

---
+++ tech-test/src/main/java/com/summer/tech/ehcache/Dog.java
@@ -0,0 +1,40 @@
+package com.summer.tech.ehcache;
+
+public class Dog {
+
+	private long id;
+	private String name;
+	private int age;
+
+	public Dog(long id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
