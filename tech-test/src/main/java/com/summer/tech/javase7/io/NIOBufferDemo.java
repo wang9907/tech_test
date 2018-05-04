6bebf19534055de81db9a97b5b89b18005e15a22
wangridong <wangridong@guokang.com>
2018-05-04 18:51:28 +0800

    NIO

--- tech-test/src/main/java/com/summer/tech/javase7/io/NIOBufferDemo.java
+++ tech-test/src/main/java/com/summer/tech/javase7/io/NIOBufferDemo.java
@@ -1,10 +1,15 @@
 package com.summer.tech.javase7.io;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
@@ -16,8 +21,9 @@ public class NIOBufferDemo {
 		// demo.byteOrder();
 		// demo.compact();
 		// demo.viewBuffer();
-		//demo.openAndWrite();
-		demo.readWriteAbsolute();
+		// demo.openAndWrite();
+		// demo.readWriteAbsolute();
+		demo.loadWebPage("http://www.baidu.com");
 	}
 
 	public void useByteBuffer() {
@@ -69,13 +75,63 @@ public class NIOBufferDemo {
 		FileChannel channel = FileChannel.open(Paths.get("absolute.txt"),
 				StandardOpenOption.CREATE, StandardOpenOption.READ,
 				StandardOpenOption.WRITE);
-		ByteBuffer writeBuffer = ByteBuffer.allocate(4).putChar('A').putChar('B');
+		ByteBuffer writeBuffer = ByteBuffer.allocate(4).putChar('A')
+				.putChar('B');
 		writeBuffer.flip();
-		channel.write(writeBuffer,1024);
+		channel.write(writeBuffer, 1024);
 		ByteBuffer readBuffer = ByteBuffer.allocate(2);
 		channel.read(readBuffer, 1026);
 		readBuffer.flip();
 		char result = readBuffer.getChar();
 		System.out.println(result);
 	}
+
+	// 使用文件通道保存网页内容
+	public void loadWebPage(String url) throws IOException {
+		try (FileChannel destChannel = FileChannel.open(
+				Paths.get("content.txt"), StandardOpenOption.CREATE,
+				StandardOpenOption.WRITE)) {
+			InputStream input = new URL(url).openStream();
+			ReadableByteChannel srcChannel = Channels.newChannel(input);
+			destChannel.transferFrom(srcChannel, 0, Integer.MAX_VALUE);
+		}
+	}
+
+	// 使用字节缓冲区进行文件复制
+	public void copyUseByteBuffer() throws IOException {
+		ByteBuffer buffer = ByteBuffer.allocate(32 * 1024);
+		try (FileChannel src = FileChannel.open(Paths.get("srcFile.txt"),
+				StandardOpenOption.READ)) {
+			FileChannel dest = FileChannel.open(Paths.get("descFile.txt"),
+					StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+			while (src.read(buffer) > -1 || buffer.position() != 0) {
+				buffer.flip();
+				dest.write(buffer);
+				buffer.compact();
+			}
+		}
+	}
+
+	// 使用文件通道进行文件复制
+	public void copyUseChannelTransfer() throws IOException {
+		try (FileChannel src = FileChannel.open(Paths.get("srcFile.txt"),
+				StandardOpenOption.READ)) {
+			FileChannel dest = FileChannel.open(Paths.get("descFile.txt"),
+					StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+			src.transferTo(0, src.size(), dest);
+		}
+	}
+
+	// 内存映射文件
+	public void mapFile() throws IOException {
+		try (FileChannel channel = FileChannel.open(Paths.get("channel.txt"),
+				StandardOpenOption.READ, StandardOpenOption.WRITE)) {
+			MappedByteBuffer buffer = channel
+					.map(FileChannel.MapMode.READ_WRITE, 0, channel.size());
+			byte b = buffer.get(1024 * 1024);
+			buffer.put(5 * 1024 * 1024, b);
+			buffer.force();
+		}
+	}
+
 }
