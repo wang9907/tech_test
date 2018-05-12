2bc47baba6994617a6052428a53c4c95e0d00a5c
wangridong <wangridong@guokang.com>
2018-05-12 14:53:45 +0800

    增加反射

--- tech-test/src/main/java/com/summer/tech/javase7/ReflectDemo.java
+++ tech-test/src/main/java/com/summer/tech/javase7/ReflectDemo.java
@@ -1,5 +1,8 @@
 package com.summer.tech.javase7;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -22,4 +25,32 @@ public class ReflectDemo {
 		method.invoke(obj, value);
 	}
 
+	// MethodType 类中的methodType方法使用
+	public void generateMethodTypes() {
+		// String.length()
+		MethodType mt1 = MethodType.methodType(int.class);
+		// String.concat(String str)
+		MethodType mt2 = MethodType.methodType(String.class, String.class);
+		// String.getChar(int srcBegin,int srcEnd,char[] dst,int dstBegin)
+		MethodType mt3 = MethodType.methodType(Void.class, int.class,
+				int.class, char[].class, int.class);
+		// String.startsWith(String prefix)
+		MethodType mt4 = MethodType.methodType(boolean.class, mt2);
+	}
+
+	public void generateMethodTypesFromDescriptor() {
+		ClassLoader cl = this.getClass().getClassLoader();
+		String descriptor = "(Ljava/lang/String;)Ljava/lang/String;";
+		MethodType mt1 = MethodType.fromMethodDescriptorString(descriptor, cl);
+	}
+
+	// 使用invokeExact方法调用方法句柄
+	public void invokeExact() throws Throwable {
+		MethodHandles.Lookup lookup = MethodHandles.lookup();
+		MethodType type = MethodType.methodType(String.class, int.class,
+				int.class);
+		MethodHandle mh = lookup.findVirtual(String.class, "substring", type);
+		String str = (String) mh.invokeExact("Hello World", 1, 3);
+		System.out.println(str);
+	}
 }
