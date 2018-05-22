eecbc4cb8e9b58ff6b8fd08d9fb03f0b5de95ad6
wangridong <wangridong@guokang.com>
2018-05-22 18:57:27 +0800

    代码自定义servlet

--- springBootDemo/src/main/java/com/summer/springboot/DemoApplication.java
+++ springBootDemo/src/main/java/com/summer/springboot/DemoApplication.java
@@ -2,9 +2,18 @@ package com.summer.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.summer.springboot.servlet.MyServlet;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new MyServlet(), "/sw/*");// ServletName默认值为首字母小写，即myServlet
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
