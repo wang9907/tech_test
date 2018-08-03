5e196bfd43c91ab3ff521a3aff05fd6355a6cd3c
wangridong <wangridong@guokang.com>
2018-08-03 23:05:11 +0800

    方法句柄

--- tech-test/src/main/java/com/summer/tech/javase7/ReflectDemo.java
+++ tech-test/src/main/java/com/summer/tech/javase7/ReflectDemo.java
@@ -44,6 +44,37 @@ public class ReflectDemo {
 		MethodType mt1 = MethodType.fromMethodDescriptorString(descriptor, cl);
 	}
 
+	public void changeMethodType() {
+		// (int,int) String
+		MethodType mt = MethodType.methodType(String.class, int.class,
+				int.class);
+		// (int,int,float) String
+		mt = mt.appendParameterTypes(float.class);
+		// (int,double,long,int,float) String
+		mt = mt.insertParameterTypes(1, double.class, long.class);
+		// (int,double,int,float) String
+		mt = mt.dropParameterTypes(2, 3);
+		// (int,double,String,float) String
+		mt = mt.changeParameterType(2, String.class);
+		// (int,double,String,float) void
+		mt = mt.changeReturnType(void.class);
+	}
+
+	public void wrapAndGeneric() {
+		// (int,double) Integer
+		MethodType mt = MethodType.methodType(Integer.class, int.class,
+				double.class);
+		// (Integer,Double)Integer
+		MethodType wrapped = mt.wrap();
+		// (int,double)int
+		MethodType unwrapped = mt.unwrap();
+		// (Object,Object) Object
+		MethodType generic = mt.generic();
+		// (int,double) Object
+		MethodType erased = mt.erase();
+
+	}
+
 	// 使用invokeExact方法调用方法句柄
 	public void invokeExact() throws Throwable {
 		MethodHandles.Lookup lookup = MethodHandles.lookup();
@@ -52,5 +83,54 @@ public class ReflectDemo {
 		MethodHandle mh = lookup.findVirtual(String.class, "substring", type);
 		String str = (String) mh.invokeExact("Hello World", 1, 3);
 		System.out.println(str);
+	}
+
+	public void normalMethod(String arg1, int arg2, int[] arg3) {
+
+	}
+
+	// 数组参数变为可变参数
+	public void asVarargsCollector() throws Throwable {
+		MethodHandles.Lookup lookup = MethodHandles.lookup();
+		MethodHandle mh = lookup.findVirtual(ReflectDemo.class, "normalMethod",
+				MethodType.methodType(void.class, String.class, int.class,
+						int[].class));
+		mh = mh.asVarargsCollector(int[].class);
+		mh.invoke(this, "Hello", 2, 3, 4, 5);
+	}
+
+	// 数组参数变为可变参数
+	public void asCollector() throws Throwable {
+		MethodHandles.Lookup lookup = MethodHandles.lookup();
+		MethodHandle mh = lookup.findVirtual(ReflectDemo.class, "normalMethod",
+				MethodType.methodType(void.class, String.class, int.class,
+						int[].class));
+		mh = mh.asCollector(int[].class, 2);
+		mh.invoke(this, "Hello", 2, 3, 4);
+	}
+
+	public void toBeSpreaded(String arg1, int arg2, int arg3, int arg4) {
+	}
+
+	// 把可变参数转换为数组
+	public void asSpreader() throws Throwable {
+		MethodHandles.Lookup lookup = MethodHandles.lookup();
+		MethodHandle mh = lookup.findVirtual(ReflectDemo.class, "toBeSpreaded",
+				MethodType.methodType(void.class, String.class, int.class,
+						int.class, int.class));
+		mh = mh.asSpreader(int[].class, 3);
+		mh.invoke(this, "Hello", new int[] { 3, 4, 5 });
+	}
+
+	public void varargsMethod(String arg1, int... args) {
+	}
+	// 最后一个方法asFixedArity是把参数长度可变的方法转换成参数长度不变的方法。经过这样的转换之后，最后一个长度可变的参数实际上就变成了对应的数组类型。在
+	public void asFixedArity() throws Throwable {
+		MethodHandles.Lookup lookup = MethodHandles.lookup();
+		MethodHandle mh = lookup.findVirtual(ReflectDemo.class,
+				"varargsMethod",
+				MethodType.methodType(void.class, String.class, int[].class));
+		mh = mh.asFixedArity();
+		mh.invoke(this, "Hello", new int[] { 2, 4 });
 	}
 }
