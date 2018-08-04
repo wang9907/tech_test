e67ccdca7703d66219b38fed443cfa53800b4049
wangridong <wangridong@guokang.com>
2018-08-05 00:14:13 +0800

    方法句柄

--- tech-test/src/main/java/com/summer/tech/javase7/ReflectDemo.java
+++ tech-test/src/main/java/com/summer/tech/javase7/ReflectDemo.java
@@ -1,8 +1,10 @@
 package com.summer.tech.javase7;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandleProxies;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.invoke.SwitchPoint;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -13,8 +15,8 @@ import javassist.tools.reflect.Sample;
 public class ReflectDemo {
 
 	public static void main(String[] args) throws Throwable {
-		multipleBindTo();
-
+		//multipleBindTo();
+		dropArguments();
 	}
 
 	// 反射API设置对象属性
@@ -227,5 +229,201 @@ public class ReflectDemo {
 	public void constant() throws Throwable {
 		MethodHandle mh = MethodHandles.constant(String.class, "Hello");
 		String value = (String) mh.invoke();// 值为"Hello"}
+	}
+	
+	//dropArguments方法的使用示例
+	public static void dropArguments()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType type=MethodType.methodType(String.class, int.class, int.class);
+		MethodHandle mhOld=lookup.findVirtual(String.class,"substring",type);
+		String value=(String)mhOld.invoke("Hello",2,3);
+		MethodHandle mhNew=MethodHandles.dropArguments(mhOld,0,float.class,String.class);
+		value=(String)mhNew.invoke(0.5f,"Ignore","Hello",2,3);
+		System.out.println(value);
+	}
+	
+	//insertArguments方法的使用示例
+	public void insertArguments()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType type=MethodType.methodType(String.class, String.class);
+		MethodHandle mhOld=lookup.findVirtual(String.class,"concat",type);
+		String value=(String)mhOld.invoke("Hello","World");
+		System.out.println(value);
+		MethodHandle mhNew=MethodHandles.insertArguments(mhOld,1,"");
+		value=(String)mhNew.invoke("Hello");//值为“Hello”
+	}
+			
+	//filterArguments方法的使用示例
+	public void filterArguments()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType type=MethodType.methodType(int.class, int.class, int.class);
+		MethodHandle mhGetLength=lookup.findVirtual(String.class,"length",MethodType.methodType(int.class));
+		MethodHandle mhTarget=lookup.findStatic(Math.class,"max",type);
+		MethodHandle mhNew=MethodHandles.filterArguments(mhTarget,0,mhGetLength,mhGetLength);
+		int value=(int)mhNew.invoke("Hello","NewWorld");//值为9
+	}
+			
+	//　foldArguments方法的使用示例
+	public static int targetMethod(int arg1,int arg2,int arg3){
+		return arg1;
+	}
+	public void foldArguments()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType typeCombiner=MethodType.methodType(int.class, int.class, int.class);
+		MethodHandle mhCombiner=lookup.findStatic(Math.class,"max",typeCombiner);
+		MethodType typeTarget=MethodType.methodType(int.class, int.class, int.class, int.class);
+		MethodHandle mhTarget=lookup.findStatic(ReflectDemo.class,"targetMethod",typeTarget);
+		MethodHandle mhResult=MethodHandles.foldArguments(mhTarget, mhCombiner);
+		int value=(int)mhResult.invoke(3,4);//输出为4
+	}
+
+	//　permuteArguments方法的使用示例
+	public void permuteArguments()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType type=MethodType.methodType(int.class, int.class, int.class);
+		MethodHandle mhCompare=lookup.findStatic(Integer.class,"compare",type);
+		int value=(int)mhCompare.invoke(3,4);//值为1
+		MethodHandle mhNew=MethodHandles.permuteArguments(mhCompare, type,1,0);
+		value=(int)mhNew.invoke(3,4);//值为1
+		MethodHandle mhDuplicateArgs=MethodHandles.permuteArguments(mhCompare, type,1,1);
+		value=(int)mhDuplicateArgs.invoke(3,4);//值为0
+	
+	}
+	
+	//　catchException方法的使用示例
+	//原始方法句柄和异常处理方法句柄的返回值类型必须是相同的,这是因为当产生异常的时候,异常处理方法句柄的返回值会作为调用的结果;
+	//而在两个方法句柄的参数方面,异常处理方法句柄的第一个参数是它所处理的异常类型,其他参数与原始方法句柄的参数相同。
+	public int handleException(Exception e, String str){
+		System.out.println(e.getMessage());
+		return 0;
+	}
+	public void catchExceptions()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType typeTarget=MethodType.methodType(int.class, String.class);
+		MethodHandle mhParseInt=lookup.findStatic(Integer.class,"parseInt",typeTarget);
+		MethodType typeHandler=MethodType.methodType(int.class, Exception.class, String.class);
+		MethodHandle mhHandler=lookup.findVirtual(ReflectDemo.class,"handleException",typeHandler).bindTo(this);
+		MethodHandle mh=MethodHandles.catchException(mhParseInt, NumberFormatException.class,mhHandler);
+		mh.invoke("Hello");
+	}
+	
+	//　guardWithTest方法的使用示例
+	public static boolean guardTest(){
+		return Math.random()>0.5;
+	}
+	public void guardWithTest()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodHandle mhTest=lookup.findStatic(ReflectDemo.class,"guardTest",MethodType.methodType(boolean.class));
+		MethodType type=MethodType.methodType(int.class, int.class, int.class);
+		MethodHandle mhTarget=lookup.findStatic(Math.class,"max",type);
+		MethodHandle mhFallback=lookup.findStatic(Math.class,"min",type);
+		MethodHandle mh=MethodHandles.guardWithTest(mhTest, mhTarget, mhFallback);
+		int value=(int)mh.invoke(3,5);//值随机为3或5
+	}
+			
+	//filterReturnValue方法的使用示例
+	public void filterReturnValue()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodHandle mhSubstring=lookup.findVirtual(String.class,"substring",MethodType.methodType(String.class, int.class));
+		MethodHandle mhUpperCase=lookup.findVirtual(String.class,"toUpperCase",MethodType.methodType(String.class));
+		MethodHandle mh=MethodHandles.filterReturnValue(mhSubstring, mhUpperCase);
+		String str=(String)mh.invoke("Hello World",5);//输出WORLD
+	}
+	
+	// 　invoker方法的使用示例
+	public void invoker()throws Throwable{
+		MethodType typeInvoker=MethodType.methodType(String.class, Object.class, int.class, int.class);
+		MethodHandle invoker=MethodHandles.invoker(typeInvoker);
+		MethodType typeFind=MethodType.methodType(String.class, int.class, int.class);
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodHandle mh1=lookup.findVirtual(String.class,"substring",typeFind);
+		MethodHandle mh2=lookup.findVirtual(ReflectDemo.class,"testMethod",typeFind);
+		String result=(String)invoker.invoke(mh1,"Hello",2,3);
+		result=(String)invoker.invoke(mh2,this,2,3);
+	}
+
+	//invoker和exactInvoker对方法句柄变换的影响
+	public void invokerTransform()throws Throwable{
+		MethodType typeInvoker=MethodType.methodType(String.class, String.class, int.class, int.class);
+		MethodHandle invoker=MethodHandles.exactInvoker(typeInvoker);
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodHandle mhUpperCase=lookup.findVirtual(String.class,"toUpperCase",MethodType.methodType(String.class));
+		invoker=MethodHandles.filterReturnValue(invoker, mhUpperCase);
+		MethodType typeFind=MethodType.methodType(String.class, int.class, int.class);
+		MethodHandle mh1=lookup.findVirtual(String.class,"substring",typeFind);
+		String result=(String)invoker.invoke(mh1,"Hello",1,4);//值为“ELL”
+	}
+
+	//使用方法句柄实现接口的示例
+	public void doSomething(){
+		System.out.println("WORK");
+	}
+	public void useMethodHandleProxy()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodHandle mh=lookup.findVirtual(ReflectDemo.class,"doSomething",MethodType.methodType(void.class));
+		mh=mh.bindTo(this);
+		Runnable runnable=MethodHandleProxies.asInterfaceInstance(Runnable.class, mh);
+		new Thread(runnable).start();
+	}
+
+	//交换点的使用示例
+	public void useSwitchPoint()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType type=MethodType.methodType(int.class, int.class, int.class);
+		MethodHandle mhMax=lookup.findStatic(Math.class,"max",type);
+		MethodHandle mhMin=lookup.findStatic(Math.class,"min",type);
+		SwitchPoint sp=new SwitchPoint();
+		MethodHandle mhNew=sp.guardWithTest(mhMin, mhMax);
+		mhNew.invoke(3,4);//值为3
+		SwitchPoint.invalidateAll(new SwitchPoint[]{sp});
+		mhNew.invoke(3,4);//值为4
+	}
+	
+	//方法句柄查找时的访问控制权限
+	private void privateMethod(){
+		System.out.println("PRIVATE");
+	}
+	public MethodHandle accessControl()throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodHandle mh=lookup.findSpecial(ReflectDemo.class,"privateMethod",MethodType.methodType(void.class),ReflectDemo.class);
+		mh=mh.bindTo(this);
+	}
+
+	//使用方法句柄实现数组操作的示例
+	private static final MethodType typeCallback=MethodType.methodType(Object.class, Object.class, int.class);
+	public static void forEach(Object[]array, MethodHandle handle)throws Throwable{
+		for(int i=0,len=array.length;i<len;i++){
+			handle.invoke(array[i],i);
+		}
+	}
+	public static Object[]map(Object[]array, MethodHandle handle)throws Throwable{
+		Object[]result=new Object[array.length];
+		for(int i=0,len=array.length;i<len;i++){
+			result[i]=handle.invoke(array[i],i);
+		}
+		return result;
+	}
+	public static Object reduce(Object[]array, Object initalValue, MethodHandle handle)throws Throwable{
+		Object result=initalValue;
+		for(int i=0,len=array.length;i<len;i++){
+			result=handle.invoke(result, array[i]);
+		}
+		return result;
+	}
+	//	使用方法句柄实现的柯里化	
+	//。柯里化的含义是对一个方法的参数值进行预先设置之后，得到一个新的方法。比如一个做加法运算的方法，本来有两个参数，通过柯里化
+	//把其中一个参数的值设为5之后，得到的新方法就只有一个参数。新方法的运行结果是用5加上这个唯一的参数的值。
+	public static MethodHandle curry(MethodHandle handle, int value){
+		return MethodHandles.insertArguments(handle,0,value);
+	}
+	public static int add(int a, int b){
+		return a+b;
+	}
+	public static int add5(int a)throws Throwable{
+		MethodHandles.Lookup lookup=MethodHandles.lookup();
+		MethodType type=MethodType.methodType(int.class, int.class, int.class);
+		MethodHandle mhAdd=lookup.findStatic(ReflectDemo.class,"add",type);
+		MethodHandle mh=curry(mhAdd,5);
+		return(int)mh.invoke(a);
 	}
 }
