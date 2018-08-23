b377cc48d73a09e77c763f36029f45ca5f43dca8
wangridong <wangridong@guokang.com>
2018-08-23 19:13:14 +0800

    控制器组件

--- springmvc/src/main/java/com/summer/tech/springmvc/controller/HelloWorldController.java
+++ springmvc/src/main/java/com/summer/tech/springmvc/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.summer.tech.springmvc.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
