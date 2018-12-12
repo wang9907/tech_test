55712ac1008fbf07e8c08a2bc6b58de2feeff7cb
wangridong <wangridong@guokang.com>
2018-12-12 18:30:38 +0800

    增加zookeeper

---
+++ tech-test/src/main/java/com/summer/tech/zookeeper/ConfigUpdater.java
@@ -0,0 +1,34 @@
+package com.summer.tech.zookeeper;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.zookeeper.KeeperException;
+
+public class ConfigUpdater {
+
+    public static final String  PATH="/config";
+
+    private ActiveKeyValueStore store;
+    private Random random=new Random();
+
+    public ConfigUpdater(String hosts) throws IOException, InterruptedException {
+        store = new ActiveKeyValueStore();
+        store.connect(hosts);
+    }
+    public void run() throws InterruptedException, KeeperException{
+        while(true){
+            String value=random.nextInt(100)+"";
+            store.write(PATH, value);
+            System.out.printf("Set %s to %s\n",PATH,value);
+            TimeUnit.SECONDS.sleep(random.nextInt(100));
+
+        }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        String server = "127.0.0.1";
+    	ConfigUpdater configUpdater = new ConfigUpdater(server);
+        configUpdater.run();
+    }
+}
