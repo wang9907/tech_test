6d050de16d9da84e05bf29ac0203013e51a21c35
wangridong <wangridong@guokang.com>
2019-09-23 13:08:32 +0800

    shiro

---
+++ shirodemo/src/main/java/com/summer/tech/shiro/servlet/UserManageServlet.java
@@ -0,0 +1,29 @@
+package com.summer.tech.shiro.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "user manage", urlPatterns = "/user")
+public class UserManageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public UserManageServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
