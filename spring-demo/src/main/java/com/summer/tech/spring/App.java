afdcc022adf00201a0fcd513ea4bf80a071bf648
wangridong <wangridong@guokang.com>
2020-04-09 19:10:27 +0800

    增加连接池相关

--- spring-demo/src/main/java/com/summer/tech/spring/App.java
+++ spring-demo/src/main/java/com/summer/tech/spring/App.java
@@ -21,9 +21,9 @@ public class App {
 
 		Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNext()) {
-
+			System.out.println(scanner.next());
 		}
-
+		scanner.close();
 		beanFactory.close();
 	}
 }
