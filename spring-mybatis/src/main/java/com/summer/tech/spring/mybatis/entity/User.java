3d369c0059a9f135e16c69a8d09f674811f8fff2
wangridong <wangridong@guokang.com>
2020-04-20 18:48:04 +0800

    mybatis增加注解和插件开发

---
+++ spring-mybatis/src/main/java/com/summer/tech/spring/mybatis/entity/User.java
@@ -0,0 +1,70 @@
+package com.summer.tech.spring.mybatis.entity;
+
+import org.omg.PortableInterceptor.ACTIVE;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class User implements Serializable{
+    private Integer id;
+
+    private String name;
+
+    private Byte gender;
+
+    private String address;
+
+    private Date birthday;
+
+//    private  Account account;
+    private List<Account> accounts;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte getGender() {
+        return gender;
+    }
+
+    public void setGender(Byte gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
--- spring-mybatis/src/test/java/com/summer/tech/spring/mybatis/SpringMybatisTest.java
+++ spring-mybatis/src/test/java/com/summer/tech/spring/mybatis/SpringMybatisTest.java
@@ -2,15 +2,17 @@ package com.summer.tech.spring.mybatis;
 
 import com.summer.tech.spring.mybatis.entity.FemaleStudent;
 import com.summer.tech.spring.mybatis.entity.MaleStudent;
-import com.summer.tech.spring.mybatis.mapper.StudentMapper;
 import com.summer.tech.spring.mybatis.entity.Student;
-import org.hamcrest.core.StringEndsWith;
+import com.summer.tech.spring.mybatis.entity.User;
+import com.summer.tech.spring.mybatis.mapper.StudentMapper;
+import com.summer.tech.spring.mybatis.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,6 +21,9 @@ public class SpringMybatisTest {
 
     @Resource
     StudentMapper studentMapper;
+
+    @Resource
+    UserMapper userMapper;
     
     @Test
     public void insertTest(){
@@ -58,4 +63,24 @@ public class SpringMybatisTest {
         }
     }
 
+    @Test
+    public void annotationUpdateTest(){
+        User user = new User();
+        user.setName("网络小兵是是 ");
+        user.setGender((byte)1);
+        user.setAddress("我来自中国");
+        user.setBirthday(new Date());
+        userMapper.insert(user);
+
+        userMapper.deleteByPrimaryKey(3);
+    }
+
+    @Test
+    public void annotationQueryTest(){
+        List<User> list = userMapper.findAll();
+        for(User user:list){
+            System.out.println(user.getName());
+            System.out.println(user.getAccounts());
+        }
+    }
 }
