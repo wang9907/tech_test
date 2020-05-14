4c868f7fff50e275549b724e43009216254d758a
wangridong <wangridong@guokang.com>
2020-05-14 14:11:21 +0800

    springboot 改为2.2.3版本

--- spring-boot1.5X/src/main/java/com/summer/tech/springboot/service/mybatis/IOrderService.java
+++
@@ -1,8 +0,0 @@
-package com.summer.tech.springboot.service.mybatis;
-
-import com.summer.tech.springboot.mybatis.model.Orders;
-import com.summer.tech.springboot.mybatis.model.Users;
-
-public interface IOrderService {
-     void addOrder(Orders orders, Users users);
-}
